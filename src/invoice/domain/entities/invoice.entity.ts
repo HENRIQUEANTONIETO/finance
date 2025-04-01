d50bac61bce7df030462ba40a03b8037db6c0ee2
Henrique <henriquebedendo10@live.com>
2025-03-31 21:22:47 -0300

    create get and setter methods in Invoice Class

--- src/invoice/domain/entities/invoice.entity.ts
+++ src/invoice/domain/entities/invoice.entity.ts
@@ -8,7 +8,47 @@ export type InvoiceProps = {
   date: Date
 }
 export class Invoice extends Entity<InvoiceProps> {
-  constructor(public readonly props: InvoiceProps) {
+  constructor(props: InvoiceProps) {
     super(props)
+  }
+
+  get description(): string {
+    return this.props.description
+  }
+
+  set description(value: string) {
+    this.props.description = value
+  }
+
+  get category(): string {
+    return this.props.category
+  }
+
+  set category(value: string) {
+    this.props.category = value
+  }
+
+  get type(): string {
+    return this.props.type
+  }
+
+  set type(value: string) {
+    this.props.type = value
+  }
+
+  get amount(): number {
+    return this.props.amount
+  }
+
+  set amount(value: number) {
+    this.props.amount = value
+  }
+
+  get date(): Date {
+    return this.props.date
+  }
+
+  set date(value: Date) {
+    this.props.date = value
   }
 }
