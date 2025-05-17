c3e617038c38c0d95d36f3e85b921b0df03ab6e9
Henrique <henriquebedendo10@live.com>
2025-05-17 19:23:23 -0300

    create layout repository

---
+++ src/layout/domain/repositories/layout.repository.ts
@@ -0,0 +1,22 @@
+import { LayoutEntity } from '../entities/layout.entity'
+import {
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+  SearchableRepositoryInterface,
+} from '@/shared/domain/repositories/searchable-repository-contracts'
+
+export namespace LayoutRepository {
+  export type Filter = string
+
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<LayoutEntity, Filter> {}
+
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      LayoutEntity,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {}
+}
