1b579676b08be07186eec200071d6d07ac190873
Henrique <henriquebedendo10@live.com>
2025-05-17 20:05:31 -0300

    create layout output dto unit test

---
+++ src/layout/application/dtos/__tests__/unit/layout-output.spec.ts
@@ -0,0 +1,14 @@
+import { LayoutEntity } from '@/layout/domain/entities/layout.entity'
+import { LayoutDataBuilder } from '@/layout/domain/testing/helpers/layout-data-builder'
+import { LayoutOutputMapper } from '../../layout-output'
+
+describe('LayoutOutput unit tests', () => {
+  it('should convert a user in output', () => {
+    const entity = new LayoutEntity(LayoutDataBuilder())
+    const spyToJson = jest.spyOn(entity, 'toJSON')
+    const sut = LayoutOutputMapper.toOutput(entity)
+
+    expect(spyToJson).toHaveBeenCalled()
+    expect(sut).toStrictEqual(entity.toJSON())
+  })
+})
