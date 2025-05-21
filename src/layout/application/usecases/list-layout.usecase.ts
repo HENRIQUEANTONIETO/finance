3f6d8c1661dcfd4055eea58608a52f6da831be9f
Henrique <henriquebedendo10@live.com>
2025-05-20 21:17:22 -0300

    create list-layout usecase

---
+++ src/layout/application/usecases/list-layout.usecase.ts
@@ -0,0 +1,33 @@
+import { LayoutRepository } from '@/layout/domain/repositories/layout.repository'
+import { LayoutOutput, LayoutOutputMapper } from '../dtos/layout-output'
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
+import { BadRequestError } from '@/shared/application/errors/bad-request-error'
+import { LayoutEntity } from '@/layout/domain/entities/layout.entity'
+import { SearchInput } from '@/shared/application/dtos/search-input'
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/application/dtos/pagination-output'
+
+export namespace ListLayoutUseCase {
+  export type Input = SearchInput
+
+  export type Output = PaginationOutput<LayoutOutput>
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private layoutRepository: LayoutRepository.Repository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const params = new LayoutRepository.SearchParams(input)
+      const searchResult = await this.layoutRepository.search(params)
+      return this.toOutput(searchResult)
+    }
+
+    private toOutput(searchResult: LayoutRepository.SearchResult): Output {
+      const items = searchResult.items.map(item => {
+        return LayoutOutputMapper.toOutput(item)
+      })
+      return PaginationOutputMapper.toOutput(items, searchResult)
+    }
+  }
+}
