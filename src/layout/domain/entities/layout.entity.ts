fe48bb1a0582be77e6d981768b3fc7ace364e649
Henrique <henriquebedendo10@live.com>
2025-05-24 18:55:27 -0300

    refactor: encapsulate properties with private setters in LayoutEntity

--- src/layout/domain/entities/layout.entity.ts
+++ src/layout/domain/entities/layout.entity.ts
@@ -41,6 +41,44 @@ export class LayoutEntity extends Entity<LayoutProps> {
     return this.props.dateField
   }
 
+  private set name(value: string) {
+    this.props.name = value
+  }
+
+  private set titleField(value: string) {
+    this.props.titleField = value
+  }
+
+  private set categoryField(value: string) {
+    this.props.categoryField = value
+  }
+
+  private set typeField(value: string) {
+    this.props.typeField = value
+  }
+
+  private set amountField(value: string) {
+    this.props.amountField = value
+  }
+
+  private set dateField(value: string) {
+    this.props.dateField = value
+  }
+
+  update(props: Partial<LayoutProps>) {
+    const updatedProps = {
+      ...this.props,
+      ...props,
+    }
+    LayoutEntity.validate(updatedProps)
+    this.name = props.name ?? this.name
+    this.titleField = props.titleField ?? this.titleField
+    this.categoryField = props.categoryField ?? this.categoryField
+    this.typeField = props.typeField ?? this.typeField
+    this.amountField = props.amountField ?? this.amountField
+    this.dateField = props.dateField ?? this.dateField
+  }
+
   static validate(props: LayoutProps): void {
     const validator = LayoutValidatorFactory.create()
     const isValid = validator.validate(props)
