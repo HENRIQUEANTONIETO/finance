481bf7c26c08d09436ea83dfc63c7f3014edc012
Henrique <henriquebedendo10@live.com>
2025-06-15 22:11:35 -0300

    feat: implement ListInvoiceUseCase for retrieving paginated invoice data

---
+++ src/invoice/application/usecases/invoice/list-invoice.usecase.ts
@@ -0,0 +1,37 @@
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
+import { SearchInput } from '@/shared/application/dtos/search-input'
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/application/dtos/pagination-output'
+import { InvoiceOutput, InvoiceOutputMapper } from '../../dtos/invoice-output'
+import { InvoiceRepository } from '@/invoice/domain/repositories/invoice.repository'
+import { type SearchResult } from '@/shared/domain/repositories/searchable-repository-contracts'
+import { type InvoiceEntity } from '@/invoice/domain/entities/invoice.entity'
+
+export namespace ListInvoiceUseCase {
+  export type Input = SearchInput
+
+  export type Output = PaginationOutput<InvoiceOutput>
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private layoutRepository: InvoiceRepository.Repository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const params = new InvoiceRepository.SearchParams(input)
+      const searchResult = await this.layoutRepository.search(params)
+      return this.toOutput(searchResult)
+    }
+
+    private toOutput(searchResult: InvoiceRepository.SearchResult): Output {
+      const items = searchResult.items.map(item => {
+        return InvoiceOutputMapper.toOutput(item)
+      })
+
+      return PaginationOutputMapper.toOutput(
+        items,
+        searchResult as SearchResult<InvoiceEntity>,
+      )
+    }
+  }
+}
