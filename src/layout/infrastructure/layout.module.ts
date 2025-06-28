651bfcdbc453f66f49672b96e0f0f08e964fcd97
Henrique <henriquebedendo10@live.com>
2025-06-28 18:33:44 -0300

    feat: add ListLayoutUseCase, UpdateLayoutUseCase, and DeleteLayoutUseCase to LayoutModule

--- src/layout/infrastructure/layout.module.ts
+++ src/layout/infrastructure/layout.module.ts
@@ -3,6 +3,9 @@ import { LayoutController } from './layout.controller'
 import { LayoutInMemoryRepository } from './database/in-memory/repositories/layout-in-memory.repository'
 import { CreateLayoutUseCase } from '../application/usecases/create-layout.usecase'
 import { LayoutRepository } from '../domain/repositories/layout.repository'
+import { ListLayoutUseCase } from '../application/usecases/list-layout.usecase'
+import { DeleteLayoutUseCase } from '../application/usecases/delete.layout.usecase'
+import { UpdateLayoutUseCase } from '../application/usecases/update-layout.usecase'
 
 @Module({
   controllers: [LayoutController],
@@ -16,6 +19,28 @@ import { LayoutRepository } from '../domain/repositories/layout.repository'
       useFactory: (layoutRepository: LayoutRepository.Repository) => {
         return new CreateLayoutUseCase.UseCase(layoutRepository)
       },
+      inject: ['LayoutRepository'],
+    },
+    {
+      provide: ListLayoutUseCase.UseCase,
+      useFactory: (layoutRepository: LayoutRepository.Repository) => {
+        return new ListLayoutUseCase.UseCase(layoutRepository)
+      },
+      inject: ['LayoutRepository'],
+    },
+    {
+      provide: UpdateLayoutUseCase.UseCase,
+      useFactory: (layoutRepository: LayoutRepository.Repository) => {
+        return new UpdateLayoutUseCase.UseCase(layoutRepository)
+      },
+      inject: ['LayoutRepository'],
+    },
+    {
+      provide: DeleteLayoutUseCase.UseCase,
+      useFactory: (layoutRepository: LayoutRepository.Repository) => {
+        return new DeleteLayoutUseCase.UseCase(layoutRepository)
+      },
+      inject: ['LayoutRepository'],
     },
   ],
 })
