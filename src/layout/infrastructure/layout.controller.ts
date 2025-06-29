5d035ddbcef8a8254fcee957c4f730695f495f14
Henrique <henriquebedendo10@live.com>
2025-06-28 22:31:23 -0300

    feat: implement LayoutController with CRUD operations for layout management

--- src/layout/infrastructure/layout.controller.ts
+++ src/layout/infrastructure/layout.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common'
+import { CreateLayoutUseCase } from '../application/usecases/create-layout.usecase'
+import { UpdateLayoutUseCase } from '../application/usecases/update-layout.usecase'
+import { ListLayoutUseCase } from '../application/usecases/list-layout.usecase'
+import { DeleteLayoutUseCase } from '../application/usecases/delete.layout.usecase'
+import { CreateLayoutDto } from './dtos/create-layout.dto'
+import { UpdateLayoutDto } from './dtos/update-layout.dto'
+import { ListLayoutDto } from './dtos/list-layout.dto'
 
 @Controller('layout')
-export class LayoutController {}
+export class LayoutController {
+  @Inject(CreateLayoutUseCase.UseCase)
+  private createLayoutUseCase: CreateLayoutUseCase.UseCase
+
+  @Inject(UpdateLayoutUseCase.UseCase)
+  private updateLayoutUseCase: UpdateLayoutUseCase.UseCase
+
+  @Inject(ListLayoutUseCase.UseCase)
+  private listLayoutUseCase: ListLayoutUseCase.UseCase
+
+  @Inject(DeleteLayoutUseCase.UseCase)
+  private deleteLayoutUseCase: DeleteLayoutUseCase.UseCase
+
+  @Post()
+  async create(@Body() createLayoutDto: CreateLayoutDto) {
+    return this.createLayoutUseCase.execute(createLayoutDto)
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateLayoutDto: UpdateLayoutDto) {
+    return this.updateLayoutUseCase.execute({ id, ...updateLayoutDto })
+  }
+
+  @Get()
+  async search(@Query() searchParams: ListLayoutDto) {
+    return this.listLayoutUseCase.execute(searchParams)
+  }
+
+  @Delete()
+  async delete(@Param('id') id: string) {
+    return this.deleteLayoutUseCase.execute({ id })
+  }
+}
