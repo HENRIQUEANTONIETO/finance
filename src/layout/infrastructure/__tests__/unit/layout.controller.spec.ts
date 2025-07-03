8ee1dd6820d286e134e1f2d269a62cb918d7c33a
Henrique <henriquebedendo10@live.com>
2025-07-03 18:40:02 -0300

    test: add definition check and update layout test to LayoutController unit tests

--- src/layout/infrastructure/__tests__/unit/layout.controller.spec.ts
+++ src/layout/infrastructure/__tests__/unit/layout.controller.spec.ts
@@ -1,6 +1,7 @@
 import { LayoutOutput } from '@/layout/application/dtos/layout-output'
 import { CreateLayoutDto } from '../../dtos/create-layout.dto'
 import { LayoutController } from '../../layout.controller'
+import { UpdateLayoutDto } from '../../dtos/update-layout.dto'
 
 describe('LayoutController unit tests', () => {
   let sut: LayoutController
@@ -8,11 +9,6 @@ describe('LayoutController unit tests', () => {
 
   beforeEach(() => {
     sut = new LayoutController()
-  })
-
-  it('should be defined', () => {
-    expect(sut).toBeDefined()
-
     props = {
       id: 'df96ae94-6128-486e-840c-b6f78abb4801',
       name: 'Inter',
@@ -22,6 +18,10 @@ describe('LayoutController unit tests', () => {
       amountField: 'Valor',
       dateField: 'Data',
     }
+  })
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined()
   })
 
   it('Should create a layout', async () => {
@@ -38,5 +38,27 @@ describe('LayoutController unit tests', () => {
 
     expect(result).toStrictEqual(output)
     expect(mockCreateLayoutUseCase.execute).toHaveBeenCalledWith(input)
+  })
+
+  it('Should update a layout', async () => {
+    const output: LayoutOutput = props
+
+    const mockUpdateLayoutUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+
+    sut['updateLayoutUseCase'] = mockUpdateLayoutUseCase as any
+
+    const input: UpdateLayoutDto = {
+      name: 'Teste',
+    }
+
+    const result = await sut.update(props.id, input)
+
+    expect(output).toMatchObject(result)
+    expect(mockUpdateLayoutUseCase.execute).toHaveBeenCalledWith({
+      id: props.id,
+      ...input,
+    })
   })
 })
