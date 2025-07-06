29c6eb99ca08bb2fd36723a2783231da5b4859e2
Henrique <henriquebedendo10@live.com>
2025-07-05 21:04:53 -0300

    Adding prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { LayoutModule } from './layout/infrastructure/layout.module'
+import { DatabaseModule } from './shared/infrastructure/database/database.module';
 
 @Module({
-  imports: [LayoutModule],
+  imports: [LayoutModule, DatabaseModule],
 })
 export class AppModule {}
