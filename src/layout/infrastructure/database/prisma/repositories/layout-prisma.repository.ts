6aa50b8dd7981f6e2e0f26952cddb629f3cd9a9d
Henrique <henriquebedendo10@live.com>
2025-08-15 20:56:34 -0300

    refactor: remove implementation of search method in LayoutPrismaRepository

--- src/layout/infrastructure/database/prisma/repositories/layout-prisma.repository.ts
+++ src/layout/infrastructure/database/prisma/repositories/layout-prisma.repository.ts
@@ -16,39 +16,7 @@ export class LayoutPrismaRepository implements LayoutRepository.Repository {
   async search(
     props: LayoutRepository.SearchParams,
   ): Promise<LayoutRepository.SearchResult> {
-    const sortable = this.sortableFields.includes(props.sort) || false
-    const orderByField = sortable ? props.sort : 'name'
-    const orderByDir = sortable ? props.sortDir : 'asc'
-
-    const count = await this.prismaService.layout.count({
-      ...(props.filter && {
-        where: {
-          name: {
-            contains: props.filter,
-            mode: 'insensitive',
-          },
-        },
-      }),
-    })
-
-    const result = await this.prismaService.layout.findMany({
-      skip: props.page && props.page > 0 ? (props.page - 1) * props.perPage : 1,
-      orderBy: { [orderByField]: orderByDir },
-      take: props.perPage && props.perPage > 0 ? props.perPage : 15,
-      // where: props.filter ? { name: props.filter } : undefined,
-    })
-
-    const models = result.map(model => new LayoutEntity(model))
-
-    return new LayoutRepository.SearchResult({
-      items: models,
-      currentPage: props.page,
-      perPage: props.perPage,
-      sort: orderByField,
-      sortDir: orderByDir,
-      total: count,
-      filter: props.filter,
-    })
+    throw new Error('Method not implemented.')
   }
   async insert(entity: LayoutEntity): Promise<void> {
     await this.prismaService.layout.create({ data: entity })
