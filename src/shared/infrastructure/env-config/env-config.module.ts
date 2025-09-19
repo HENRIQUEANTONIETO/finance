77e587e8ec940a042900657c0eec696515c15197
Henrique <henriquebedendo10@live.com>
2025-09-19 19:03:31 -0300

    feat: refactor EnvConfigModule to extend ConfigModule and update DatabaseModule to be global

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -8,18 +8,11 @@ import { join } from 'node:path'
   providers: [EnvConfigService],
   exports: [EnvConfigService],
 })
-export class EnvConfigModule {
-  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
-    return {
-      module: EnvConfigModule,
-      imports: [
-        ConfigModule.forRoot({
-          ...options,
-          envFilePath: [join(__dirname, `../../../../.env.${process.env.NODE_ENV}`)],
-        }),
-      ],
-      providers: [EnvConfigService],
-      exports: [EnvConfigService],
-    }
+export class EnvConfigModule extends ConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): Promise<DynamicModule> {
+    return super.forRoot({
+      ...options,
+      envFilePath: [join(__dirname, `../../../../.env.${process.env.NODE_ENV}`)],
+    })
   }
 }
