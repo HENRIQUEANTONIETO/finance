a6ff8e55df770354b98b46ddb85473e672964d1e
Henrique <henriquebedendo10@live.com>
2025-09-21 08:04:08 -0300

    add global config for error filters

---
+++ src/global-config.ts
@@ -0,0 +1,21 @@
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { ConflictErrorFilter } from './shared/infrastructure/exception-filters/conflict-error/conflict-error.filter'
+import { NotFoundErrorFilter } from './shared/infrastructure/exception-filters/not-found-error/not-found-error.filter'
+
+export function applyGlobalConfig(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+  app.useGlobalFilters(new ConflictErrorFilter(), new NotFoundErrorFilter())
+}
