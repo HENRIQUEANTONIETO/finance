1deb265d708b55e516b13ff9704f9c87277f7312
Henrique <henriquebedendo10@live.com>
2025-11-02 07:44:56 -0300

    fix: remove InvoiceController from AppModule controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common'
 import { LayoutModule } from './layout/infrastructure/layout.module'
 import { DatabaseModule } from './shared/infrastructure/database/database.module'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
-import { InvoiceController } from './invoice/infrastructure/invoice.controller'
 import { InvoiceModule } from './invoice/infrastructure/invoice.module'
 
 @Module({
   imports: [EnvConfigModule, LayoutModule, DatabaseModule, InvoiceModule],
-  controllers: [InvoiceController],
+  controllers: [],
 })
 export class AppModule {}
