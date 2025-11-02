eb8fc28e9cd05dcd3a52b40cc7319fd079f30bb7
Henrique <henriquebedendo10@live.com>
2025-11-02 06:34:07 -0300

    feat: add InvoiceModule and InvoiceController to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common'
 import { LayoutModule } from './layout/infrastructure/layout.module'
 import { DatabaseModule } from './shared/infrastructure/database/database.module'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
+import { InvoiceController } from './invoice/infrastructure/invoice.controller'
+import { InvoiceModule } from './invoice/infrastructure/invoice/invoice.module';
 
 @Module({
-  imports: [EnvConfigModule, LayoutModule, DatabaseModule],
+  imports: [EnvConfigModule, LayoutModule, DatabaseModule, InvoiceModule],
+  controllers: [InvoiceController],
 })
 export class AppModule {}
