917e479b1a977513034549cfd2683734657b56ba
Henrique <henriquebedendo10@live.com>
2025-11-02 06:34:25 -0300

    fix: correct import path for InvoiceModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { LayoutModule } from './layout/infrastructure/layout.module'
 import { DatabaseModule } from './shared/infrastructure/database/database.module'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 import { InvoiceController } from './invoice/infrastructure/invoice.controller'
-import { InvoiceModule } from './invoice/infrastructure/invoice/invoice.module';
+import { InvoiceModule } from './invoice/infrastructure/invoice.module'
 
 @Module({
   imports: [EnvConfigModule, LayoutModule, DatabaseModule, InvoiceModule],
