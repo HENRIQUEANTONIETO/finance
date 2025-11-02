8990c46cbdde8207b18fdc0972307103e5346372
Henrique <henriquebedendo10@live.com>
2025-11-02 07:45:03 -0300

    feat: add validation decorators for year in ListInvoiceDto

--- src/invoice/infrastructure/dtos/list-invoice.dto.ts
+++ src/invoice/infrastructure/dtos/list-invoice.dto.ts
@@ -1,10 +1,16 @@
 import { ListInvoiceUseCase } from '@/invoice/application/usecases/invoice/list-invoice.usecase'
 import { SortDirection } from '@/shared/domain/repositories/searchable-repository-contracts'
+import { Type } from 'class-transformer'
+import { IsNotEmpty, IsNumber } from 'class-validator'
 
 export class ListInvoiceDto implements ListInvoiceUseCase.Input {
   page?: number
   perPage?: number
   sort?: string
   sortDir?: SortDirection
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Type(() => Number)
   year: number
 }
